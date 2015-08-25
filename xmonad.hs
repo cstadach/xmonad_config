129b8e0a85588f37bffc967800b3159a6bfd7508
Christian Stadach <christian@stadach.com>
2015-08-25 08:47:26 +0200

    added preffered workspaces for applications

--- xmonad.hs
+++ xmonad.hs
@@ -57,7 +57,7 @@ myNumlockMask   = mod2Mask
 --
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
-myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces    = ["1:server","2:work","3:mail","4:audio","5","6","7","8","9"]
  
 -- Border colors for unfocused and focused windows, respectively.
 --
@@ -232,7 +232,8 @@ myLayout = tiled ||| Mirror tiled ||| Full
 -- 'className' and 'resource' are used below.
 --
 myManageHook = composeAll
-    [ className =? "MPlayer"        --> doFloat
+    [ className =? "Thunderbird"    --> doShift "3:mail"
+    , className =? "Spotify"        --> doShift "4:audio"
     , className =? "Gimp"           --> doFloat
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore ]
