1c03df73b6626c0a2261d865fb0de57b7c349e66
Christian Stadach <christian@stadach.com>
2016-05-20 08:24:11 +0200

    added toggle touchpad to xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -178,6 +178,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((0              , 0x1008ff02     ), spawn "xbacklight +2")
     , ((0              , 0x1008ff03     ), spawn "xbacklight -2")
     , ((0              , 0xff61         ), spawn "/home/christian/.xmonad/bin/toggle-wifi")
+    , ((controlMask,        xK_m     ), spawn "/home/christian/.xmonad/bin/toggle-touchpad")
     ]
     ++
  
